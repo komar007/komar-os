e7ad9c950c0591dda8e2243fcbccd4c328ae79e2
Michal Trybus <M.Trybus@adbglobal.com>
2025-07-16 12:12:06 +0200

    feat(firefox): defined socks proxy for work container

--- homes/modules/firefox/containers/work.nix
+++ homes/modules/firefox/containers/work.nix
@@ -1,10 +1,31 @@
 { lib, pkgs, ... }:
+let
+  ff-utils = import ../utils.nix { inherit lib; };
+  firefox-addons = pkgs.nur.repos.rycee.firefox-addons;
+in
 {
   programs.firefox.profiles."default-release" = {
     containers.work = {
       id = 2;
       color = "red";
       icon = "briefcase";
+    };
+
+    extensions.settings."${ff-utils.extensionId firefox-addons.container-proxy}".settings = {
+      proxies = [
+        {
+          id = "efc535d3-740d-4b69-8374-6bf4e303b661";
+          title = "work";
+          type = "socks";
+          host = "localhost";
+          port = 9999;
+          doNotProxyLocal = true;
+          proxyDNS = true;
+        }
+      ];
+      relations = {
+        "firefox-container-2" = [ "efc535d3-740d-4b69-8374-6bf4e303b661" ];
+      };
     };
   };
 }
