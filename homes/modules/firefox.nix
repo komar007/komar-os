055fbe1ab1e61ad6933731aaa782e063ba93b863
Michal Trybus <M.Trybus@adbglobal.com>
2025-07-16 10:39:49 +0200

    feat(firefox): added home container with container-proxy socks5 proxy config

--- homes/modules/firefox.nix
+++ homes/modules/firefox.nix
@@ -80,10 +80,32 @@ in
 
     containersForce = true;
     containers = {
+      home = {
+        id = 1;
+        color = "blue";
+        icon = "chill";
+      };
       work = {
         id = 2;
         color = "red";
         icon = "briefcase";
+      };
+    };
+
+    extensions.settings."${ffAddonId firefox-addons.container-proxy}".settings = {
+      proxies = [
+        {
+          id = "8b1964af-23a7-43c7-b7cb-828ad3f2ab31";
+          title = "home";
+          type = "socks";
+          host = "localhost";
+          port = 9998;
+          doNotProxyLocal = true;
+          proxyDNS = true;
+        }
+      ];
+      relations = {
+        "firefox-container-1" = [ "8b1964af-23a7-43c7-b7cb-828ad3f2ab31" ];
       };
     };
 
@@ -134,5 +156,7 @@ in
       container-proxy
       dark-mode-webextension
     ];
+
+    extensions.force = true;
   };
 }
