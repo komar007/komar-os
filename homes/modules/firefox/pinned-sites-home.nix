694892231b103a0119f9ad40ad21e69abd2dd299
Michal Trybus <komar007@gmail.com>
2025-08-21 14:26:30 +0200

    fix(firefox): fixed voron pin

--- homes/modules/firefox/pinned-sites-home.nix
+++ homes/modules/firefox/pinned-sites-home.nix
@@ -4,7 +4,7 @@
       { label = "Gmail"; url = "https://mail.google.com/mail/u/0/#inbox"; }
       { label = "Google Keep"; url = "https://keep.google.com"; }
       { label = "Home Assistant"; url = "http://192.168.88.5:8123/lovelace-test/default_view"; }
-      { label = "Voron 2.4R2"; url = "https://192.168.88.94"; }
+      { label = "Voron 2.4R2"; url = "http://192.168.88.94"; }
       { }
       { }
       { }
