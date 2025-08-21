ae90e05dd272b89ba502127e1253811763b10c8f
Michal Trybus <komar007@gmail.com>
2025-08-21 14:26:11 +0200

    feat(firefox): pinned gmail.com

--- homes/modules/firefox/pinned-sites-home.nix
+++ homes/modules/firefox/pinned-sites-home.nix
@@ -1,6 +1,7 @@
 { lib, pkgs, ... }: {
   programs.firefox.profiles."default-release".settings = {
     "browser.newtabpage.pinned" = [
+      { label = "Gmail"; url = "https://mail.google.com/mail/u/0/#inbox"; }
       { label = "Google Keep"; url = "https://keep.google.com"; }
       { label = "Home Assistant"; url = "http://192.168.88.5:8123/lovelace-test/default_view"; }
       { label = "Voron 2.4R2"; url = "https://192.168.88.94"; }
