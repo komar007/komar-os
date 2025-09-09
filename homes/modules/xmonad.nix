b528ebeae06c63947ba50f82f60ed96c6bac546d
Michal Trybus <M.Trybus@adbglobal.com>
2025-09-09 15:03:39 +0200

    feat(xmonad): added window shadows

--- homes/modules/xmonad.nix
+++ homes/modules/xmonad.nix
@@ -16,6 +16,12 @@
         method = "dual_kawase";
         strength = 5;
       };
+      blur-background-exclude = ''window_type *= "menu"'';
+      shadow = true;
+      shadow-exclude = ''_NET_WM_STRUT || window_type *= "menu"'';
+      shadow-offset-x = -18;
+      shadow-offset-y = -18;
+      crop-shadow-to-monitor = true;
     };
   };
 }
