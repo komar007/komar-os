6caaa622c1634daad3fd7b48f63ef213fe2e5e0f
Michal Trybus <komar007@gmail.com>
2025-09-18 23:51:49 +0200

    fix(session-chooser): added missing ncurses dependency (tput)

--- homes/modules/session-chooser/default.nix
+++ homes/modules/session-chooser/default.nix
@@ -2,7 +2,7 @@
   home.packages = [
     (pkgs.writeShellApplication {
       name = "session-chooser";
-      runtimeInputs = with pkgs; [ fzf openssh ];
+      runtimeInputs = with pkgs; [ fzf openssh ncurses ];
       text = builtins.readFile ./session-chooser.sh;
     })
   ];
