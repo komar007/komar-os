f8089a7c9f37d7b27a3fdcca61f9bdf0f89710d7
Michal Trybus <komar007@gmail.com>
2025-09-19 18:36:03 +0200

    feat(x11): set wallpaper

--- homes/modules/xmonad.nix
+++ homes/modules/xmonad.nix
@@ -6,7 +6,13 @@
     dzen2
     xmobar
     htop
+    xwallpaper
   ];
+
+  home.file.".wallpaper.png".source = builtins.fetchurl {
+    url = "https://raw.githubusercontent.com/D3Ext/aesthetic-wallpapers/refs/heads/main/images/minimal_gradient.png";
+    sha256 = "sha256:1rp0w67a7v3fivjlh3ima2agbis6r1gj822mfln6z5n056c415jn";
+  };
 
   imports = [
     ./session-chooser
