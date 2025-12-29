ccf92b0ae4354b059d2891aeed6cc9b0d44df0f0
Michal Trybus <komar007@gmail.com>
2025-12-29 22:17:52 +0100

    fix(ssh): set identity file only for framework (it breaks other machines while sops is not used there)

--- homes/framework/ssh/default.nix
+++ homes/framework/ssh/default.nix
@@ -2,6 +2,10 @@
 {
   home.file.".ssh/id_ed25519.pub".source = ./id_ed25519.pub;
 
+  programs.ssh.matchBlocks."*" = {
+    identityFile = "/run/secrets/users/komar/ssh_key";
+  };
+
   programs.ssh.matchBlocks.thinkcentre = {
     host = "thinkcentre";
     hostname = "192.168.88.2";
--- homes/modules/ssh.nix
+++ homes/modules/ssh.nix
@@ -5,7 +5,6 @@
     enableDefaultConfig = false;
 
     matchBlocks."*" = {
-      identityFile = "/run/secrets/users/komar/ssh_key";
       serverAliveInterval = 100;
       serverAliveCountMax = 3;
     };
