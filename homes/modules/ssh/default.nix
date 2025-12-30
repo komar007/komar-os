03ab438b5f64ac77a6e7fad4a232b7e7c0e591fa
Michal Trybus <komar007@gmail.com>
2025-12-30 18:50:41 +0100

    fix(ssh): wrong authorized_keys perms

--- homes/modules/ssh/default.nix
+++ homes/modules/ssh/default.nix
@@ -17,11 +17,21 @@
     };
   };
 
-  config.home.file.".ssh/authorized_keys" =
+  # This is a workaround, see https://github.com/nix-community/home-manager/issues/3090#issuecomment-3341948190.
+  # openssh will not accept any permissions > 0600, which is what we get from HM.
+  config.home.file.".ssh/authorized_keys_link" =
     let
       keys = map lib.strings.trim config.ssh.authorizedKeys;
     in
     lib.mkIf (lib.lists.length keys > 0) {
       text = lib.concatStringsSep "\n" keys + "\n";
+      onChange = ''
+        if [ -f ~/.ssh/authorized_keys ] && ! [ -f ~/.ssh/authorized_keys.old_no_hm ]; then
+          mv ~/.ssh/authorized_keys ~/.ssh/authorized_keys.old_no_hm
+        fi
+        cat ~/.ssh/authorized_keys_link > ~/.ssh/authorized_keys
+        chmod 600 ~/.ssh/authorized_keys
+      '';
+      force = true;
     };
 }
