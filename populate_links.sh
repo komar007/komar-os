871f09b3732c85ccc0fbd2344a1744d116c2d7ba
Michal Trybus <M.Trybus@adbglobal.com>
2026-03-19 10:10:08 +0100

    feat: retire gdb config

--- populate_links.sh
+++ populate_links.sh
@@ -2,9 +2,7 @@
 
 # The directories below contain legacy (non-home-manager) configuration files
 # They must be linked manually or with this script
-for d in xorg gdb; do
-	for f in "$d"/*; do
-		ln -s "$(pwd)/$f" "$HOME/.$(basename "$f")"
-	done
+for f in xorg/*; do
+	ln -s "$(pwd)/$f" "$HOME/.$(basename "$f")"
 done
 ln -s "$(pwd)/slock" ~/.slock
