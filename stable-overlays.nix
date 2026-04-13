25034c1ab511a51a984dd2fe0cdb3e1e7473cba5
Michal Trybus <M.Trybus@adbglobal.com>
2026-04-13 12:52:05 +0200

    refactor(comma): move COMMA_PICKER from bashrc to a wrapper

--- stable-overlays.nix
+++ stable-overlays.nix
@@ -6,10 +6,24 @@
   comma,
   ...
 }:
+let
+  commaEnvWrappedOverlay = (
+    final: prev: {
+      comma = prev.comma.overrideAttrs (old: {
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ final.makeWrapper ];
+        postFixup = (old.postFixup or "") + ''
+          wrapProgram $out/bin/comma \
+            --set-default COMMA_PICKER "${final.lib.getExe final.fzf}"
+        '';
+      });
+    }
+  );
+in
 [
   nur.overlays.default
   nixgl.overlay
   rust-overlay.overlays.default
   fshf.overlays.default
   comma.overlays.default
+  commaEnvWrappedOverlay
 ]
