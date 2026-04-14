c132228bd33f39edc1eee010d71c8e96cb0f3516
Michal Trybus <M.Trybus@adbglobal.com>
2026-04-14 14:53:02 +0200

    feat(comma): sort by package name length, then alphabetically

--- homes/hm-stable-overlays.nix
+++ homes/hm-stable-overlays.nix
@@ -8,12 +8,29 @@
 }:
 let
   commaEnvWrappedOverlay = (
-    final: prev: {
+    final: prev:
+    let
+      shortFirstFzf = final.writeShellApplication {
+        name = "shortFirstFzf";
+        runtimeInputs = with final; [
+          coreutils
+          gawk
+          fzf
+        ];
+        text = ''
+          awk '{ print length, $0 }' |
+            sort -k1,1n -k2,2r |
+            cut -d' ' -f2- |
+            fzf
+        '';
+      };
+    in
+    {
       comma = prev.comma.overrideAttrs (old: {
         nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ final.makeWrapper ];
         postFixup = (old.postFixup or "") + ''
           wrapProgram $out/bin/comma \
-            --set-default COMMA_PICKER "${final.lib.getExe final.fzf}"
+            --set-default COMMA_PICKER "${final.lib.getExe shortFirstFzf}"
         '';
       });
     }
