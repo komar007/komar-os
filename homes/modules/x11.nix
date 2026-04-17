bb2f0be831ac606d6135f0de7660d26030b9e5ab
Michal Trybus <M.Trybus@adbglobal.com>
2026-04-17 14:08:22 +0200

    feat(x11): introduce clip & unclip

--- homes/modules/x11.nix
+++ homes/modules/x11.nix
@@ -1,4 +1,29 @@
 { pkgs, ... }:
+let
+  clip = pkgs.writeShellApplication {
+    name = "clip";
+    runtimeInputs = with pkgs; [
+      mktemp
+      xclip
+      file
+    ];
+    text = ''
+      T=$(mktemp)
+      cleanup() { rm "$T"; }
+      trap cleanup EXIT
+      cat > "$T"
+      mime=$(file -b --mime-type "$T")
+      xclip -selection clipboard -t "$mime" -i "$T"
+    '';
+  };
+  unclip = pkgs.writeShellApplication {
+    name = "unclip";
+    runtimeInputs = [ pkgs.xclip ];
+    text = ''
+      xclip -selection clipboard -o -
+    '';
+  };
+in
 {
   home.pointerCursor = {
     gtk.enable = true;
@@ -7,15 +32,20 @@
     package = pkgs.vanilla-dmz;
   };
 
-  home.packages = with pkgs; [
-    xclip
-    xorg.xset
+  home.packages =
+    (with pkgs; [
+      xclip
+      xorg.xset
 
-    geeqie
-    feh
-    scrot
-    imagemagick
-    gnuplot
-    xcolor
-  ];
+      geeqie
+      feh
+      scrot
+      imagemagick
+      gnuplot
+      xcolor
+    ])
+    ++ [
+      clip
+      unclip
+    ];
 }
