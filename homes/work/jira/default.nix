118b462d818d9973701ab8c62648f8074fe9a866
Michal Trybus <M.Trybus@adbglobal.com>
2026-07-07 09:19:37 +0200

    feat(jira): migrate to cloud

--- homes/work/jira/default.nix
+++ homes/work/jira/default.nix
@@ -1,7 +1,4 @@
 { config, ... }:
-let
-  utils = import ../../modules/jira/utils.nix { };
-in
 {
   imports = [
     ../../modules/jira
@@ -9,41 +6,21 @@ in
 
   sops.secrets."password/jira_adb" = { };
 
-  jira.host = "jira.adbglobal.com";
-  jira.login = "m.trybus";
+  jira.host = "adbglobal.atlassian.net";
+  jira.login = "m.trybus@adbglobal.com";
   jira.password = config.sops.placeholder."password/jira_adb";
 
   jira.config = {
-    auth_type = "bearer";
+    auth_type = "basic";
     board = {
-      id = 548;
+      id = 220;
       name = "PRISME Roadmap";
       type = "scrum";
     };
-    installation = "Local";
+    installation = "Cloud";
     project = {
       key = "PRISME";
     };
     timezone = "Europe/Warsaw";
-    issue.types = [
-      (utils.task 1 "Bug")
-      (utils.task 2 "New Feature")
-      (utils.task 3 "Task")
-      (utils.task 4 "Improvement")
-      (utils.subtask 5 "Sub-task")
-      (utils.task 6 "Question")
-      (utils.task 7 "Project Issue")
-      (utils.task 8 "FIRE & Emergency Issue")
-      (utils.subtask 9 "Sub-Issue")
-      (utils.task 16 "Epic")
-      (utils.task 17 "Story")
-      (utils.subtask 18 "Technical task")
-      (utils.task 30 "Milestone")
-      (utils.subtask 33 "SW release milestone")
-      (utils.task 10103 "Risk")
-      (utils.task 10500 "Customer Opportunity")
-      (utils.task 10601 "Report")
-      (utils.task 10900 "Prototype")
-    ];
   };
 }
